9556b3d30a3be525a2c67c34deace81acefbfb71
N Zavala <noezh.25@gmail.com>
2024-08-05 17:57:15 -0600

    Dashboard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { CoursesComponent } from './features/dashboard/courses/courses.component';
+import { StudentsComponent } from './features/dashboard/students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'courses',
+        component: CoursesComponent,
+      },
+      {
+        path: 'students',
+        component: StudentsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
